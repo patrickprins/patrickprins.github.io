f0d01ac469114052b3468cac90f4498fb2f5d5a5
patrickprins <patrickprins@users.noreply.github.com>
2017-11-09 23:26:22 +0100

    simplecart attempt 5

---
+++ study2/js/theme.js
@@ -0,0 +1,21 @@
+simpleCart({
+    checkout: {
+      type: "SendForm",
+      url: "p.s.prins2@students.uu.nl"
+    },
+    
+    cartStyle: 'table'
+    
+    cartColumns: [
+    { view: "image" , attr: "thumb" , label: false }
+    { attr: "name" , label: "Productnaam" } ,
+    { attr: "price" , label: "Prijs", view: 'currency' } ,
+    { view: "decrement" , label: false , text: "-" } ,
+    { attr: "quantity" , label: "Aantal" } ,
+    { view: "increment" , label: false , text: "+" } ,
+    { attr: "total" , label: "Totaalbedrag", view: 'currency' } ,
+    { view: "remove" , text: "Verwijder" , label: false }
+    ]
+    
+});
+
