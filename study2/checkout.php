6484c60b438ddf24071297244d6dc538cae88cf3
patrickprins <patrickprins@users.noreply.github.com>
2017-11-14 12:46:38 +0100

    heel veel haha

---
+++ study2/checkout.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Click&Eat | Winkelwagen</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Super Market Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+    
+<!-- simplecart -->
+<script src="js/simpleCart.js"></script>
+<script src="js/theme.js"></script>
+    
+<link href='//fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->  
+</head>
+	
+<body>
+<!-- header -->
+	<div class="agileits_header">
+		<div class="container">
+			<div class="w3l_offers">
+				<p>HET EXPERIMENT IS NU BEZIG!</p>
+			</div>
+			<div class="agile-login">
+				<ul>>
+					<li><a href="faq.html">Help</a></li>
+					
+				</ul>
+			</div>
+			
+          
+           <!--Winkelwagentje in header-->
+            <div class="product_list_header">
+                <a href="checkout.html">
+                <div class="w3view-cart">
+                    <button class="w3view-cart"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button> 
+                </div>
+                 
+                <div class="winkelwagentekst">
+                
+                <p><span class="simpleCart_quantity" class="hyperlinkoranje"></span> producten</p>
+                <span class="simpleCart_total"></span>
+            </div>
+                </a>
+            </div>
+  
+ 
+            
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+
+	<div class="logo_products">
+		<div class="container">
+		<div class="w3ls_logo_products_left1" align="right"><!--<img src="images/gratis-thuisbezorgd.png">--></div>
+			<div class="w3ls_logo_products_left">
+                <a href="index.html"><img src="images/logo.png"></a>
+			</div>
+		<div class="w3l_search">
+			<form action="#" method="post">
+				<input type="search" name="Search" placeholder="Zoek een product..." required="">
+				<button type="submit" class="btn btn-default search" aria-label="Left Align">
+					<i class="fa fa-search" aria-hidden="true"> </i>
+				</button>
+				<div class="clearfix"></div>
+			</form>
+		</div>
+			
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //header -->
+<!-- navigation -->
+	<div class="navigation-agileits">
+		<div class="container">
+			<nav class="navbar navbar-default">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header nav_2">
+								<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div> 
+							<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="index.html" class="act">Home</a></li>	
+									<!-- Mega Menu -->
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Brood & bakkerij<b class="caret"></b></a>
+										<ul class="dropdown-menu multi-column columns-3">
+											<div class="row">
+												<div class="multi-gd-img">
+													<ul class="multi-column-dropdown">
+														<h6><a href="broodbakkerij.html" style="color: #fe9126">Brood & bakkerij</a></h6>
+														<li><a href="broodbakkerij-belegdebroodjes.html">Belegde broodjes</a></li>
+														<li><a href="broodbakkerij-broodjes">Broodjes</a></li>
+														<li><a href="broodbakkerij-hartigesnacks.html">Hartige snacks</a></li>
+													</ul>
+												</div>	
+												
+											</div>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Fastfood & salades<b class="caret"></b></a>
+										<ul class="dropdown-menu multi-column columns-3">
+											<div class="row">
+												<div class="multi-gd-img">
+													<ul class="multi-column-dropdown">
+                                                        <h6><a href="fastfoodsalades.html" style="color: #fe9126">Fastfood & salades</a></h6>
+														<li><a href="fastfoodsalades-burgers.html">Burgers</a></li>
+														<li><a href="fastfoodsalades-pizzaheel.html">Pizza (heel)</a></li>
+														<li><a href="fastfoodsalades-pizzapunt.html">Pizza (punt)</a></li>
+														<li><a href="fastfoodsalades-salades.html">Salades</a></li>
+													</ul>
+												</div>
+												
+												
+											</div>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Chips, koek, snoep & borrelhapjes<b class="caret"></b></a>
+										<ul class="dropdown-menu multi-column columns-3">
+											<div class="row">
+												<div class="multi-gd-img">
+													<ul class="multi-column-dropdown">
+                                                        <h6><a href="cksb.html" style="color: #fe9126">Chips, koek, snoep & borrelhapjes</a></h6>
+														<li><a href="cksb-chips.html">Chips</a></li>
+														<li><a href="cksb-koek.html">Koek</a></li>
+														<li><a href="cksb-snoep.html">Snoep</a></li>
+														<li><a href="cksb-zoutjes.html">Zoutjes</a></li>
+														<li><a href="cksb-borrelhapjes.html">Borrelhapjes</a></li>
+													</ul>
+												</div>
+												
+											</div>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dranken<b class="caret"></b></a>
+										<ul class="dropdown-menu multi-column columns-3">
+											<div class="row">
+												<div class="multi-gd-img">
+													<ul class="multi-column-dropdown">
+                                                        <h6><a href="dranken.html" style="color: #fe9126">Dranken</a></h6>
+														<li><a href="dranken-versesappen.html">Verse sappen</a></li>
+														<li><a href="dranken-warmedranken.html">Warme dranken</a></li>
+														<li><a href="dranken-frisdranken.html">Frisdranken</a></li>
+														<li><a href="dranken-bier.html">Bier</a></li>
+														<li><a href="dranken-wijn.html">Wijn</a></li>
+														<li><a href="dranken-sterkedranken.html">Sterke dranken</a></li>
+													</ul>
+												</div>
+							
+											</div>
+										</ul>
+									</li>
+								</ul>
+							</div>
+							</nav>
+			</div>
+		</div>
+		
+<!-- //navigation -->
+<!-- breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<ol class="breadcrumb breadcrumb1">
+				<li><a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+				<li class="active">Winkelwagen</li>
+			</ol>
+		</div>
+	</div>
+<!-- //breadcrumbs -->
+<!-- checkout -->
+	<div class="checkout">
+		<div class="container">
+			<h1>WINKELWAGEN</h1><br>           
+            <br>           
+            <div class="simpleCart_items" class="winkelwagentabel"></div>
+            <div class="cartTotal">
+                <br><br>
+                <p>Totaalbedrag: </p><div class="simpleCart_total"></div>
+                </div>
+                     <br><a href="javascript:;" class="simpleCart_checkout">Bevestigen</a>
+                                
+                                <?php
+                                $content = "some text here";
+                                $fp = fopen("myText.txt","wb");
+                                fwrite($fp,$content);
+                                fclose($fp);
+                                ?>
+            
+         </div>                     
+    </div>
+
+
+<!-- //checkout -->
+<!-- //footer -->
+<div class="footer">
+		<div class="container">
+			<div class="w3_footer_grids">
+				<div class="col-md-3 w3_footer_grid">
+					<h3><img src="images/logo_footer.png"></h3>
+					<ul class="address">
+						<li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>Voedselplein 12, <span>Amersfoort</span></li>
+						<li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:p.s.prins2@students.uu.nl">info@clickandeat.com</a></li>
+						<li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>033 765 4321</li>
+					</ul>
+				</div>
+				<div class="col-md-3 w3_footer_grid">
+					<h3>Informatie</h3>
+					<ul class="info">
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="about.html">Over ons</a></li>
+                        <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="faq.html">Vaakgestelde vragen</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="contact.html">Contact</a></li>
+						<!-- <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="short-codes.html">Short Codes</a></li> -->
+						<!-- <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="products.html">Special Products</a></li> -->
+					</ul>
+				</div>
+				<div class="col-md-3 w3_footer_grid">
+					<h3>Categorieën</h3>
+					<ul class="info"> 
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="groceries.html">Brood & bakkerij</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="household.html">Fastfood & salades</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="personalcare.html">Chips, koek, snoep & borrelhapjes</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="packagedfoods.html">Dranken</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 w3_footer_grid">
+					<h3>Profiel</h3>
+					<ul class="info"> 
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="products.html">Winkel</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="checkout.html">Winkelwagen</a></li>
+						<!-- <li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="login.html">Login</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="registered.html">Create Account</a></li> -->
+					</ul>
+					
+					
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		
+		<div class="footer-copy">
+			
+			<div class="container">
+				<p>© 2017 Click&Eat.</p>
+			</div>
+		</div>
+		
+	</div>	
+	<div class="footer-botm">
+			<div class="container">
+				<div class="w3layouts-foot">
+					<ul>
+						<li><a href="#" class="w3_agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="w3_agile_dribble"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="w3_agile_vimeo"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="payment-w3ls">	
+					<img src="images/card.png" alt=" " class="img-responsive">
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+<!-- //footer -->	
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<!-- top-header and slider -->
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+<script src="js/minicart.min.js"></script>
+<script>
+	// Mini Cart
+	paypal.minicart.render({
+		action: '#'
+	});
+
+	if (~window.location.search.indexOf('reset=true')) {
+		paypal.minicart.reset();
+	}
+</script>
+<!-- main slider-banner -->
+<script src="js/skdslider.min.js"></script>
+<link href="css/skdslider.css" rel="stylesheet">
+<script type="text/javascript">
+		jQuery(document).ready(function(){
+			jQuery('#demo1').skdslider({'delay':5000, 'animationSpeed': 2000,'showNextPrev':true,'showPlayButton':true,'autoSlide':true,'animationType':'fading'});
+						
+			jQuery('#responsive').change(function(){
+			  $('#responsive_wrapper').width(jQuery(this).val());
+			});
+			
+		});
+</script>	
+<!-- //main slider-banner --> 
+
+</body>
+</html>
